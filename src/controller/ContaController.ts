d710e176b72093de07107e8c18f2d9b296ffb317
Daniel Ribeiro <dann.ribeiroo@gmail.com>
2025-11-13 01:47:25 -0300

    feat: call account controller on menu

--- src/controller/ContaController.ts
+++ src/controller/ContaController.ts
@@ -4,39 +4,77 @@ import { colors } from "../utils/Colors";
 
 export class ContaController implements ContaRepository {
     private listaContas: Array<Conta> = new Array<Conta>();
-    private numero: number = 0;
+    private numero = 0;
 
     procurarPorNumero(numero: number): void {
-        throw new Error("Method not implemented.");
+        let buscaConta = this.buscaNoArray(numero);
+
+        if (buscaConta != null) {
+            buscaConta.visualizar();
+        } else {
+            console.log(colors.fg.red, "\nA Conta número: " + numero
+                + " não foi encontrada!", colors.reset
+            );
+        }
     }
+
     listarTodas(): void {
         this.listaContas.map((conta) => {
             conta.visualizar();
         })
     }
+
     cadastrar(conta: Conta): void {
         this.listaContas.push(conta);
         console.log(colors.fg.green, "\nA Conta número: " + conta.numero +
             " foi criada com sucesso!", colors.reset)
     }
+
     atualizar(conta: Conta): void {
-        throw new Error("Method not implemented.");
+        console.log("CONTA: ", JSON.stringify(conta));
+        let buscaConta = this.buscaNoArray(conta.numero);
+
+        if (buscaConta != null) {
+            this.listaContas[this.listaContas.indexOf(buscaConta)] = conta;
+            console.log(colors.fg.green, "\nA Conta número: " + conta.numero +
+                " foi atualizada com sucesso!", colors.reset);
+        }
     }
+
     deletar(numero: number): void {
-        throw new Error("Method not implemented.");
+        let buscaConta = this.buscaNoArray(numero);
+
+        if (buscaConta != null) {
+            this.listaContas.splice(this.listaContas.indexOf(buscaConta), 1);
+            console.log(colors.fg.green, "\nA Conta número: " + numero +
+                " foi deletada com sucesso!", colors.reset);
+        }
     }
+
+
     sacar(numero: number, valor: number): void {
         throw new Error("Method not implemented.");
     }
+
     depositar(numero: number, valor: number): void {
         throw new Error("Method not implemented.");
     }
+
     transferir(numeroOrigem: number, numeroDestino: number, valor: number): void {
         throw new Error("Method not implemented.");
     }
 
     public gerarNumero(): number {
-        return Math.random();
+        return ++this.numero;
     }
 
+    public buscaNoArray(numero: number): Conta | null {
+        const contaEncontrada = this.listaContas.find((conta) => {
+        return conta.numero === numero;
+    });
+
+    console.log(contaEncontrada);
+
+    return contaEncontrada || null;
+    }
 }
